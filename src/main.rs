ad516e17c566a72ec54d319290c365e22b507558
CharlieKolb <charlie.kolb@outlook.de>
2020-09-27 21:00:11 +0100

    Remove old code

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-use std::fs::File;
-use std::io::BufReader;
-use std::io::prelude::*;
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
 
-use std::iter::FromIterator;
-use std::collections::HashSet;
+mod token;
 
-const DEFAULT_PROGRAM_PATH : &'static str = "./programs/helloWorld.lorx";
+const DEFAULT_PROGRAM_PATH: &'static str = "./programs/helloWorld.lox";
 
-#[macro_use]
-extern crate lazy_static;
-
-
-lazy_static! {
-    static ref KEYWORDS: HashSet<&'static str> = HashSet::from_iter([ "print", "if", "elif", "else" ].iter().cloned());
-    static ref SYMBOLS: HashSet<&'static str> = HashSet::from_iter([ ":", "=", "<", ">", "<=", ">=", "==", "!", "!=", "&", "|", "and", "or" ].iter().cloned());
-
-}
 
 fn read_program() -> std::io::Result<String> {
     // Prints each argument on a separate line
@@ -24,7 +14,9 @@ fn read_program() -> std::io::Result<String> {
     //     panic!("Expected exactly one argument!");
     // }
 
-    let path = env::args().nth(1).unwrap_or(DEFAULT_PROGRAM_PATH.to_owned());
+    let path = env::args()
+        .nth(1)
+        .unwrap_or(DEFAULT_PROGRAM_PATH.to_owned());
 
     let file = File::open(path)?;
     let mut buf_reader = BufReader::new(file);
@@ -33,49 +25,9 @@ fn read_program() -> std::io::Result<String> {
     Ok(contents)
 }
 
-       
-#[derive(Debug)]
-enum Token {
-    Keyword(String),
-    Name(String),
-    Symbol(String),
-}
-
-fn tokenize(program: &String) -> Vec<Token> {
-    let mut tokens : Vec<Token> = vec![];
-    let mut curr_word = String::new();
-    let mut next_escaped = false;
-    for c in program.chars() {
-        if next_escaped {
-            next_escaped = false;
-            curr_word.push(c);
-            continue
-        }
-
-        match c {
-            | '\\' => { next_escaped = true; continue }
-            | ' ' => {
-                if curr_word.len() > 0 {
-                    tokens.push(
-                        match &curr_word {
-                           | word if KEYWORDS.contains(&word.as_str()) => Token::Keyword(word.to_string()),
-                           | word if SYMBOLS.contains(&word.as_str()) => Token::Symbol(word.to_string()),
-                           | word => Token::Name(word.to_string()) // ToDo: Check for valid name and panic otherwise
-                        }
-                    );
-                    curr_word.clear();
-                }
-            }
-            | _ => { curr_word.push(c) }
-        }
-    }
-    tokens
-}
-
-
-fn main() -> std::io::Result<()> { 
+fn main() -> std::io::Result<()> {
     let text = read_program()?;
-    let tokens = tokenize(&text);
-    println!("{:?}", tokens);
+    // let tokens = tokenize(&text);
+    // println!("{:?}", tokens);
     Ok(())
 }
