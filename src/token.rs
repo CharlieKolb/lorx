96cb66154333aa46bfd00248bdb2df1d67c02a5e
CharlieKolb <charlie.kolb@outlook.de>
2020-09-27 20:49:41 +0100

    Add token file

---
+++ src/token.rs
@@ -0,0 +1,59 @@
+#[derive(Debug, Clone)]
+pub enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier(String),
+    Text(String), // Instead of String to avoid name conflict
+    Number(String),
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct TokenPosition {
+    start: isize,
+    end: isize,
+}
+
+#[derive(Debug, Clone)]
+pub struct Token(TokenType, TokenPosition);
