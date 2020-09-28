a84604dbc8334066e119331c340faed17796372f
CharlieKolb <charlie.kolb@outlook.de>
2020-09-28 05:44:51 +0100

    Finish scanner

--- src/scanner.rs
+++ src/scanner.rs
@@ -3,6 +3,8 @@ use crate::token::*;
 use std::iter::Peekable;
 use std::str::Chars;
 
+extern crate itertools;
+
 #[derive(Debug, Clone)]
 pub struct ScanState<'a> {
     chars: Peekable<Chars<'a>>,
@@ -12,9 +14,17 @@ pub struct ScanState<'a> {
     line: usize,
 }
 
+fn is_digit(c: char) -> bool {
+    c >= '0' && c <= '9'
+}
+
+fn is_alpha(c: char) -> bool {
+    (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '_'
+}
+
 fn scan_token<'a>(
     c: char,
-    iter: &mut Peekable<Chars<'a>>,
+    iter: &mut itertools::MultiPeek<Chars<'a>>,
     current: &mut usize,
     line: &mut usize,
 ) -> Token {
@@ -28,7 +38,7 @@ fn scan_token<'a>(
     };
 
     Token {
-        tokenType: match c {
+        token_type: match c {
             ' ' | '\r' | '\t' => TokenType::Whitespace(c.to_string()),
             '\n' => {
                 *line += 1;
@@ -59,34 +69,113 @@ fn scan_token<'a>(
                             comment.push(c);
                             iter.next();
                         } else {
-                            break
+                            iter.reset_peek();
+                            break;
                         }
                         *current += 1;
-
                     }
                     TokenType::Comment(comment)
                 } else {
                     TokenType::Slash
                 }
-            },
-            
+            }
+            '"' => {
+                let mut res = String::new();
+                while let Some(&c) = iter.peek() {
+                    if c == '"' {
+                        iter.reset_peek();
+                        break;
+                    }
+
+                    if c == '\n' {
+                        *line += 1;
+                    }
+                    res.push(c);
+                    iter.next();
+                }
+
+                // closing quote missing
+                if iter.peek() == None {
+                    panic!();
+                }
+
+                // skip closing quote
+                iter.next();
+
+                TokenType::Text(res)
+            }
+            c if is_digit(c) => {
+                let mut res = c.to_string();
+                while let Some(&next_c) = iter.peek() {
+                    if !is_digit(next_c) {
+                        iter.reset_peek();
+                        break;
+                    }
+
+                    res.push(next_c);
+                    iter.next();
+                }
+
+                if iter.peek() == Some(&'.') && is_digit(*iter.peek().unwrap_or(&'x')) {
+                    iter.next();
+                    res.push('.');
+
+                    while let Some(&next_c) = iter.peek() {
+                        if !is_digit(c) {
+                            iter.reset_peek();
+                            break;
+                        }
+
+                        res.push(next_c);
+                        iter.next();
+                    }
+                }
+
+                if let Ok(f) = res.parse::<f64>() {
+                    TokenType::Number(f)
+                } else {
+                    panic!();
+                    // todo return an Error token here
+                }
+            }
+            c if is_alpha(c) => {
+                let mut res = c.to_string();
+                while let Some(&c) = iter.peek() {
+                    if !(is_alpha(c) || is_digit(c)) {
+                        iter.reset_peek();
+                        break;
+                    }
+
+                    res.push(c);
+                    iter.next();
+                }
+
+                if let Some(keyword_token) = keyword_to_token_type(&res) {
+                    keyword_token
+                } else {
+                    TokenType::Identifier(res)
+                }
+            }
             // todo handle unsupported characters with an error type or something
-            _ => panic!()
+            _ => panic!(),
         },
-        lexeme: c.to_string(),
         line: line.clone(),
     }
 }
 
 pub fn scan_tokens<'a>(input_chars: Chars<'a>) -> Vec<Token> {
-    let mut input_peekable = input_chars.peekable();
+    let mut input_mpeek = itertools::multipeek(input_chars);
 
     let mut current = 0;
     let mut line = 1;
 
     let mut out = vec![];
-    while let Some(c) = input_peekable.next() {
-        out.push(scan_token(c, &mut input_peekable, &mut current, &mut line))
+    while let Some(c) = input_mpeek.next() {
+        out.push(scan_token(c, &mut input_mpeek, &mut current, &mut line))
     }
+    out.push(Token {
+        token_type: TokenType::Eof,
+        line,
+    });
     out
 }
--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,46 @@
+pub fn keyword_to_token_type(s: &String) -> Option<TokenType> {
+    match s.as_str() {
+        "and" => Some(TokenType::And),
+        "class" => Some(TokenType::Class),
+        "else" => Some(TokenType::Else),
+        "false" => Some(TokenType::False),
+        "for" => Some(TokenType::For),
+        "fun" => Some(TokenType::Fun),
+        "if" => Some(TokenType::If),
+        "nil" => Some(TokenType::Nil),
+        "or" => Some(TokenType::Or),
+        "print" => Some(TokenType::Print),
+        "return" => Some(TokenType::Return),
+        "super" => Some(TokenType::Super),
+        "this" => Some(TokenType::This),
+        "true" => Some(TokenType::True),
+        "var" => Some(TokenType::Var),
+        "while" => Some(TokenType::While),
+        _ => None,
+    }
+}
+
+// lazy_static! {
+//     pub static ref KEYWORDS: HashMap<String, TokenType> = [
+//         ("and",    TokenType::And),
+//         ("class",  TokenType::Class),
+//         ("else",   TokenType::Else),
+//         ("false",  TokenType::False),
+//         ("for",    TokenType::For),
+//         ("fun",    TokenType::Fun),
+//         ("if",     TokenType::If),
+//         ("nil",    TokenType::Nil),
+//         ("or",     TokenType::Or),
+//         ("print",  TokenType::Print),
+//         ("return", TokenType::Return),
+//         ("super",  TokenType::Super),
+//         ("this",   TokenType::This),
+//         ("true",   TokenType::True),
+//         ("var",    TokenType::Var),
+//         ("while",  TokenType::While),
+//     ].iter().map(|(k, v)| (k.to_string(), v.clone())).collect();
+// }
+
 #[derive(Debug, Clone)]
 pub enum TokenType {
     // Single-character tokens.
@@ -25,8 +68,8 @@ pub enum TokenType {
 
     // Literals.
     Identifier(String),
-    Text(String),   // Instead of String to avoid name conflict
-    Number(String), // or isize, maybe?
+    Text(String), // Instead of String to avoid name conflict
+    Number(f64),
     Comment(String),
     Whitespace(String),
 
@@ -53,7 +96,6 @@ pub enum TokenType {
 
 #[derive(Debug, Clone)]
 pub struct Token {
-    pub tokenType: TokenType,
-    pub lexeme: String,
+    pub token_type: TokenType,
     pub line: usize,
 }
